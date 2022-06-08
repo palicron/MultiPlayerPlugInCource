25b2fe2816b4b7aa62853d90bc735b86e47447de
JsPalacios <jspalacios@teravisiongames.com>
2022-06-08 14:01:34 -0500

    Class 57 finish

--- Blaster/Source/Blaster/Private/Character/BlasterCharacter.cpp
+++ Blaster/Source/Blaster/Private/Character/BlasterCharacter.cpp
@@ -192,6 +192,13 @@ void ABlasterCharacter::AimOffSet(float DeltaTime)
 	}
 
 	AO_Pitch = GetBaseAimRotation().Pitch;
+	if(AO_Pitch>90.f && !IsLocallyControlled())
+	{
+		//Map pithc from [270,360) to [-90,0)
+		FVector2d InRange(270.f,360.f);
+		FVector2d OutRange(-90.f,0.f);
+		AO_Pitch = FMath::GetMappedRangeValueClamped(InRange,OutRange,AO_Pitch);
+	}
 }
 
 void ABlasterCharacter::SetOverlappingWeapon(AWeapon* weapon)
