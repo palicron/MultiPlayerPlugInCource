85aa29ad356a1405916467e52dbad0e01ff9ee45
JsPalacios <elgranpalicron@gmail.com>
2022-09-02 16:29:38 -0500

    create shootgun calss

---
+++ Blaster/Source/Blaster/Public/Weapon/ShotGun.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blaster/Weapon/HitScanWeapon.h"
+#include "ShotGun.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLASTER_API AShotGun : public AHitScanWeapon
+{
+	GENERATED_BODY()
+	
+};
